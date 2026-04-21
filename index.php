8588b8058107607aa8ab13ae1ce684dc4aee6948
rishi raj singh <142165249+devilrishi69@users.noreply.github.com>
2026-04-21 22:52:37 +0530

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+// Root entry point: Redirect to login page
+header("Location: login.php");
+exit();
+?>
+
+</body>
+</html>
